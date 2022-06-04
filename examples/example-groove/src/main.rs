e2872a90a864dae2d2e0fda43668476c3f37a701
Cameron Bytheway <bythewc@amazon.com>
2022-06-04 00:39:54 -0600

    update example

--- examples/example-groove/src/main.rs
+++ examples/example-groove/src/main.rs
@@ -1,39 +1,3 @@
-# euphony
-
-Music composition environment for programmers
-
-## Installation
-
-Install `euphony` with `cargo`:
-
-```sh
-$ cargo install euphony-cli
-```
-
-## Getting started
-
-First, initialize a workspace:
-
-```sh
-$ euphony ws init my-workspace
-$ cd my-workspace
-```
-
-Next, create a composition project in the workspace
-
-```sh
-$ euphony ws new cool-song
-```
-
-Finally, start the multi-track player
-
-```sh
-$ euphony play cool-song
-```
-
-## Example
-
-```rust
 use euphony::prelude::*;
 use samples::dirt;
 use western::*;
@@ -116,4 +80,3 @@ async fn bass(interval: Interval) {
 
     sink.fin();
 }
-```
